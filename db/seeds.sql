fe1fde2cec34ffd24a4adad1b8ded483c884bc68
Pete McLaughlin <Petemcl1978@gmail.com>
2019-07-09 11:54:54 -0400

    fixing bugs hope this works!~

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 ---seeds Burgers into Database
 
-INSERT INTO burgers(burger_name, devoured, date)
+INSERT INTO burgers_db.burgers(burger_name, devoured, date)
 VALUES("Big Mac", false, "2019-07-01 04:20:00"),
  ("Whooper", false, "2019-07-01 04:20:01"),
   ("Cheezburger", false, "2019-07-01 04:20:02"),
